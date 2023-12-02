a0d9b1ee533329b169e534470004d9437c7ba54d
Henrik <henrik.forsgren@ri.se>
2023-12-02 10:51:59 +0100

    first commit

---
+++ src/ciphertext.rs
@@ -0,0 +1,43 @@
+use tfhe::integer::{RadixCiphertext};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct FheAsciiChar{
+    pub ctxt: RadixCiphertext,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FheString{
+    pub str: Vec<FheAsciiChar>,
+}
+
+impl FheString {
+
+    pub fn encrypt_str(
+        client_key: &RadixClientKey,
+        s: &str,
+    ) -> Result<FheString, Box<dyn std::error::Error>> {
+        if !s.is_ascii() {
+            return Err("content contains non-ascii characters".into());
+        }
+        Ok(s.as_bytes()
+            .iter()
+            .map(|byte| client_key.encrypt(*byte as u64))
+            .collect())
+    }
+
+    pub fn decrypt(encstring: FheString) -> str{
+        let mut plainstring = vec![];
+    }
+
+    pub fn contains(&self, s: &str) -> bool{
+        
+    }
+
+    pub fn starts_with(&self, s: &str) -> bool{
+
+    }
+    
+    pub fn ends_with(&self, s: &str) -> bool{
+        
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::{env, error};
+use tfhe::prelude::*;
+use tfhe::{generate_keys, set_server_key, ConfigBuilder, FheUint16, FheUint32, FheUint8};
+
+mod cipertext;
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    // Basic configuration to use homomorphic integers
+    let config = ConfigBuilder::all_disabled()
+        .enable_default_integers()
+        .build();
+
+    // Key generation
+    let (client_key, server_keys) = generate_keys(config);
+
+    // Get string from command line
+    // let args: Vec<String> = env::args().collect();
+    // let content = &args[1];
+    // let pattern = &args[2];
+
+    // Parse string and encode to ASCII representation
+    
+
+    let clear_a = 1344u32;
+    let clear_b = 5u32;
+
+    // Encrypting the input data using the (private) client_key
+    // FheUint32: Encrypted equivalent to u32
+    let mut encrypted_a = FheUint16::try_encrypt(clear_a, &client_key)?;
+    let encrypted_b = FheUint16::try_encrypt(clear_b, &client_key)?;
+
+
+    // assert_eq!(clear_res, 1_u8);
+
+    Ok(())
+}
+
+    // Encrypt string
+}
